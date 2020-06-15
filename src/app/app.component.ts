010af8bd98ec762a0172265b2fec865d1e0619ac
Academy_intern <academy_intern@Academy-interns-MacBook-Air.local>
2020-06-15 16:36:30 +0200

    created new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'school-management-app';
+  elements: any = [
+    {id: 1, first: 'Mark', last: 'Otto', email: '@mdo'},
+    {id: 2, first: 'Jacob', last: 'Thornton', email: '@fat'},
+    {id: 3, first: 'Larry', last: 'the Bird', email: '@twitter'},
+  ];
+  headElements = ['ID', 'First', 'Last', 'Email'];
 }
---
+++ src/app/components/school-details/school-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-school-details',
+  templateUrl: './school-details.component.html',
+  styleUrls: ['./school-details.component.css']
+})
+export class SchoolDetailsComponent implements OnInit {
+  
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
